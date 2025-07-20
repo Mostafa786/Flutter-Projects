395bdc810193bce255dc15ee3a63fd7d19fae754
Mostafa Mahmoud <121934683+Mostafa786@users.noreply.github.com>
2025-07-20 17:36:21 +0300

    Delete lib directory

--- lib/cubit/button_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bloccourse/cubit/button_state.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class ButtonCubit extends Cubit<ButtonState>{
-  ButtonCubit():super(ButtonInit());
-  void buttonStatus(note){
-      emit(ButtonUpdate(note));
-  }
-}
